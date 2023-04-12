c39084977e54f91fc8627e3ed2ad7eb06fda597f
kyletaylor94 <kyletaylorrules94@gmail.com>
2023-04-12 20:38:20 +0200

    Initial Commit

---
+++ CrypToSwiFt/CrypToSwiFtApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrypToSwiFtApp.swift
+//  CrypToSwiFt
+//
+//  Created by Turdesán Csaba on 2023. 04. 12..
+//
+
+import SwiftUI
+
+@main
+struct CrypToSwiFtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
